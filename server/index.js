16159e2bdc640cd2ab45f56058a7e35534b4e89f
kirjav <89679609+kirjav@users.noreply.github.com>
2025-07-02 21:15:14 -0500

    secure user auth finished

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // server/index.js
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
 
 // Load env variables
 dotenv.config();
