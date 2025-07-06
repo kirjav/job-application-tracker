7268044ba29e74a786d17ba850dfa95f744e1d87
kirjav <89679609+kirjav@users.noreply.github.com>
2025-07-05 23:27:27 -0500

    updated regex pw check complexity

--- server/utils/passwordUtils.js
+++ server/utils/passwordUtils.js
@@ -1,8 +1,8 @@
 // utils/passwordUtils.js
 
 function isPasswordComplex(password) {
-  // Example: at least 8 characters, includes a number and a letter
-  const complexityRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
+  // 1 uppercase, 1 lowercase, 1 digit, 1 special character
+  const complexityRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   return complexityRegex.test(password);
 }
 
