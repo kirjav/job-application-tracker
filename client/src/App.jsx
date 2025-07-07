90d07513585d5c9c0546a56dd11f97b4ed1d80d6
kirjav <89679609+kirjav@users.noreply.github.com>
2025-07-07 00:11:00 -0500

    core functionality component structure.. not content yet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+import ApplicationForm from "./components/ApplicationForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <div>
+      <h1>Hello</h1>
+    </div>
+    /*<div style={{ padding: "2rem" }}>
+      <h2>Register</h2>
+      <RegisterForm />
+
+      <hr />
+
+      <h2>Login</h2>
+      <LoginForm />
+
+      <hr />
+
+      <h2>Create Application</h2>
+      <ApplicationForm />
+    </div>*/
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/ApplicationForm.jsx
@@ -0,0 +1,6 @@
+function ApplicationForm(){
+
+}
+
+
+export default ApplicationForm;
